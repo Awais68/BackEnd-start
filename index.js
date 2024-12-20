a963fc034f9e0e06916fedf7a1313cb175f0765b
Mr. Awais <161300531+Awais68@users.noreply.github.com>
2024-12-20 15:34:06 +0500

    mongodb Connected

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import morgan from "morgan";
-import userRoutes from "./routers/users.js";
-import courses from "./routers/courses.js";
+import "dotenv/config";
+import mongoose, { connect } from "mongoose";
+
 
 const app = express();
 const PORT = 4001;
 
+// console.log("MONGODBUrl=>", process.env.MONGODBURI);
+
 app.use(morgan("tiny"));
-app.use(express.json()); // ye middleware  ap ki body ko json me convert kr deta hai
+app.use(express.json()); // ye middleware  ap ki body ko json me convert kr deta hai.
+
+mongoose
+  .connect(process.env.MONGODBURI)
+  .then(() => console.log("mongoDB Connected "))
+  .catch((err) => console.log("err=>", err));
 
 app.get("/", (req, res) => res.send("Server is running..."));
 
